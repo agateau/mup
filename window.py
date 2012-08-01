bd9f90a71eed3ded8298cf02b09e20b9b36f5513
Aurélien Gâteau <agateau@kde.org>
2012-08-01 10:15:30 +0200

    Add editing support

--- window.py
+++ window.py
@@ -1,3 +1,5 @@
+import subprocess
+
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
@@ -7,10 +9,23 @@ class Window(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
 
+        self._filename = ""
+
+        self.setupToolBar()
         self.view = View()
         self.view.mdUrlClicked.connect(self.load)
         self.setCentralWidget(self.view)
 
+    def setupToolBar(self):
+        toolBar = self.addToolBar(self.tr("Main"))
+        action = toolBar.addAction(self.tr("Edit"))
+        action.triggered.connect(self.edit)
+
     def load(self, filename):
+        self._filename = filename
         self.setWindowTitle(filename + " - mdview")
         self.view.load(filename)
+
+    def edit(self):
+        # FIXME: Use configurable editor
+        subprocess.call(["gvim", self._filename])
