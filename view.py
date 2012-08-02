32903a4355331b731292cff8c0c00e2d4b738494
Aurélien Gâteau <agateau@kde.org>
2012-08-02 11:45:41 +0200

    Use python markdown module
    This makes it possible to have a baseUrl which is the dir of the .md file
    instead of the dir of view.html

--- view.py
+++ view.py
@@ -4,6 +4,8 @@ from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from PyQt4.QtWebKit import *
 
+import markdown
+
 class WebPage(QWebPage):
     def javaScriptConsoleMessage(self, msg, lineNumber, sourceID):
         print "JsConsole(%s:%d): %s" % (sourceID, lineNumber, msg)
@@ -30,39 +32,25 @@ class View(QWidget):
         page.setLinkDelegationPolicy(QWebPage.DelegateAllLinks)
         page.linkClicked.connect(self._openUrl)
         self.view.setPage(page)
-        page.mainFrame().addToJavaScriptWindowObject("qtWindow", self)
-        url = QUrl("file://" + self.dataDir + "/view.html")
-        self.view.setUrl(url)
 
     def load(self, filename):
         self.filename = filename
         self.reload()
 
     def reload(self):
-        if os.path.exists(self.filename):
-            filename = self.filename
-        else:
+        filename = unicode(self.filename)
+        if not os.path.exists(filename):
             filename = os.path.join(self.dataDir, "placeholder.md")
         txt = open(filename).read()
-        self.setText(QString(txt))
 
-    # Definition of "text" property, used on the JS side
-    textChanged = pyqtSignal(QString)
-
-    def setText(self, value):
-        if self._text != value:
-            self._text = value
-            self.textChanged.emit(self._text)
-
-    @pyqtProperty(QString, fset=setText, notify=textChanged)
-    def text(self):
-        return self._text
-    # /Definition
+        html = markdown.markdown(txt)
+        baseUrl = QUrl(os.path.dirname(filename) + "/")
+        self.view.setHtml(html, baseUrl)
 
     def _openUrl(self, url):
         if url.scheme() == "internal":
             self.internalUrlClicked.emit(url)
-        if url.isLocalFile() and url.path().endsWith(".md"):
+        if url.scheme() in ("file", "") and url.path().endsWith(".md"):
             self.loadRequested.emit(url.path())
         else:
             QDesktopServices.openUrl(url)
